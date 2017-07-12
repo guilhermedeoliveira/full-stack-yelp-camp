432cc2acbd2b26f33d0b86c3ed234143d009c603
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-07-12 15:33:46 -0300

    Basic setup boilerplate

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express'),
       app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-  res.send('This is the landing page');
+  res.render('landing_page');
+});
+
+app.get('/camps', function(req, res) {
+  let camps = [
+    { name: "Camp1" },
+    { name: "Camp2" },
+    { name: "Camp3" }
+  ];
+  res.render('camps', { camps });
 });
 
 app.listen(3000, function() {
