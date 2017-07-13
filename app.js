a793530ebc6f57dfdd0fb1fe6a4a40f468478b30
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-07-13 11:51:47 -0300

    Configure initial routes and templates

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require('express'),
-      app = express();
+      app = express(),
+      bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+const camps = [
+  { name: "Camp1" },
+  { name: "Camp2" },
+  { name: "Camp3" }
+];
 
 app.get('/', function(req, res) {
   res.render('landing_page');
 });
 
 app.get('/camps', function(req, res) {
-  let camps = [
-    { name: "Camp1" },
-    { name: "Camp2" },
-    { name: "Camp3" }
-  ];
   res.render('camps', { camps });
+});
+
+app.post('/camps', function(req, res) {
+  const name = req.body.camp;
+  const newCamp = { name };
+  camps.push(newCamp);
+  res.redirect('/camps');
+});
+
+app.get('/camps/new', function(req, res) {
+  res.render('new');
 });
 
 app.listen(3000, function() {
